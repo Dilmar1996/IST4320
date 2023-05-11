6d950f5f01440e0c3c5873ab13768b5402721f97
Dilmar1996 <dilmarfuentes1@gmail.com>
2023-05-11 03:23:29 +0000

    The function just asks for your name and age small_hw_improve.py

---
+++ Activity_5/small_hw_improve.py
@@ -0,0 +1,8 @@
+#! /usr/local/bin/python3
+print("What is your name?")
+name= input("My name is:?")
+print(name)
+print("Hello", name, "nice to meet you")
+num = input("how old are you?:")
+print("Wow! that is", num, "!")
+                   
